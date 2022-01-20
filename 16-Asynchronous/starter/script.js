e7c85d8d4c3632e542ad4a5695a0656a74e44d3c
Guillermoo <guillermo.cano.es@gmail.com>
2022-01-20 12:47:44 +0100

    265 Runing promisses on parallel

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -435,3 +435,27 @@ const whereAmI = async function () {
     console.err(`${err.message}`);
   }
 })(); */
+
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    //De esta manera se cargar'an en cascada, una detras de otra
+    // const [data1] = await getJSON(`https://restcountries.com/v2/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v2/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v2/name/${c3}`);
+
+    //console.log([data1.capital, data2.capital, data3.capital]);
+    //Se cargan todas a la vez.
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v2/name/${c1}`),
+      getJSON(`https://restcountries.com/v2/name/${c2}`),
+      getJSON(`https://restcountries.com/v2/name/${c3}`),
+    ]);
+
+    console.log(data.map(d => d[0].capital));
+    data.forEach(c => console.log(c[0].name));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+get3Countries('portugal', 'canada', 'tanzania');
