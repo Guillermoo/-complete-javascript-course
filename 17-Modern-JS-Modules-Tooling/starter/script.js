bde5ffb0dddeaa8f5af7a864da1fbeefe4d5bae4
Guillermoo <guillermo.cano.es@gmail.com>
2022-01-27 16:09:00 +0100

    Quitando node_modules

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -7,7 +7,7 @@
 // addToCart('bread', 4);
 // console.log(price, totalQuantity);
 
-console.log('Importing module');
+/* console.log('Importing module');
 
 // import * as ShoppingCart from './shoppingCart.js';
 
@@ -21,3 +21,76 @@ add('bread', 5);
 add('apples', 4);
 
 console.log(cart); // cart no est'a vacio, quiere decir que la variable cart no es una copia, es el mismo objeto.
+
+// console.log('Start Fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+// Aunque es await el consolelog Something no se ejecutara hasta que se cargue todo el data.
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost); // Nos devovlera un promise, lo siguiente lo soluciona
+
+//Lo soluciona pero no es la mejor opcion
+//lastPost.then(last => console.log(last));
+
+//Esta es la mejor opcion
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
+// Conclusion, mejor no usar el await fuera de un async */
+
+// ----------- Patron module ----------------
+/* const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantiy = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to the cart`);
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantiy,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+
+console.log(ShoppingCart2.shippingCost); // undefined, no se puede acceder porque la funcion no permite. Cosas de los enclousures */
+
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 3 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+
+console.log(stateClone);
+
+console.log(stateDeepClone);
