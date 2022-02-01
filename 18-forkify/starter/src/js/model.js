169a49fa43a53a43ecb635f8ff1499f2f40d7648
griyermo <guillermo.cano.es@gmail.com>
2022-02-01 21:51:55 +0100

    #293 Event Handlers, subscriber pattern

--- 18-forkify/starter/src/js/model.js
+++ 18-forkify/starter/src/js/model.js
@@ -1,20 +1,14 @@
+import { API_URL } from './config';
+import { getJSON } from './helpers';
+
 export const state = {
   recipe: {},
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-    console.log(res);
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-
+    const data = await getJSON(`${API_URL}/${id}`);
     console.log(data);
-
     const { recipe } = data.data;
 
     state.recipe = {
@@ -27,8 +21,7 @@ export const loadRecipe = async function (id) {
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients,
     };
-    console.log(state.recipe);
   } catch (err) {
-    alert(err);
+    console.error(`${err} 💥💥💥💥`);
   }
 };
